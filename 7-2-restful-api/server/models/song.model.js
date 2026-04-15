98baf3e6835be427c2a67ee2d805d28d3c969b7a
Mazen Osama <elkafrawy@MacBook-Air-Mazen.local>
2026-04-15 09:19:46 +0300

    Done

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema({
+  title:  { type: String, required: true },
+  artist: { type: String, required: true },
+  year:   { type: Number },
+},{timestamps: true});
 
 export const Song = mongoose.model("Song", songSchema);
